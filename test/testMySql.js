ced3066e077755946320bc08ad913a4a4e9510c8
kavi-appveen <kavi@appveen.com>
2022-12-09 01:25:52 +0530

    MySql Testing changes

--- test/testMySql.js
+++ test/testMySql.js
@@ -1,4 +1,4 @@
-const CRUD = require('../index').mssql;
+const CRUD = require('../index').mysql;
 // const utils = require('./utils');
 
 const jsonSchema = require('./sample.json');
@@ -6,28 +6,26 @@ const jsonSchema = require('./sample.json');
 
 (async () => {
     try {
-        const msCrud = await new CRUD({
-            connectionString: ''
-        });
+        const msCrud = await new CRUD();
         await msCrud.connect();
 
         // Test custom SQL Query
         const result = await msCrud.sqlQuery('SELECT 1 + 1 AS solution');
-        console.log('Result :: ', result.recordset[0].solution);
+        console.log('Result :: ', result[0][0].solution);
 
 
         // Create table in MS SQL if it doesn't exist
-        const apis = msCrud.table('employee', jsonSchema);
-        // await apis.createTable({});
+        // const apis = msCrud.table('employee', jsonSchema);
+        // await apis.createTable();
 
 
         // Get total count of all records in table
-        const totalCount = await apis.count({});
-        console.log('Total records count :: ', totalCount);
+        // const totalCount = await apis.count({});
+        // console.log('Total records count :: ', totalCount);
 
 
         // Count records based on filter
-        // const count = await apis.count({"name": "Ka"});
+        // const count = await apis.count({"name": "Kavi"});
         // console.log('Records count :: ', count);
 
 
@@ -48,23 +46,23 @@ const jsonSchema = require('./sample.json');
         // console.log('Ordered Records :: ', orderedRecords);
 
         // List records with orderBy, limit and offset
-        const limitRecords = await apis.list({"sort": "-_id", "select": "_id,name,email,password", "count": 1, "page": 0, "filter": {"name": "Kavi"}});
-        console.log('Limit Records :: ', limitRecords);
+        // const limitRecords = await apis.list({"sort": "_id", "select": "_id,name,email,password", "count": 5, "page": 0, "filter": {"name": "Kavi"}});
+        // console.log('Limit Records :: ', limitRecords);
 
         // Show record by ID
-        // const showRecord = await apis.show("EMP1001");
+        // const showRecord = await apis.show("TES1001");
         // console.log('Show Record :: ', showRecord);
 
         // Show record by ID and select fields
-        // const showSelectRecord = await apis.show("EMP1002", {"select": "name,email,password,_id"});
+        // const showSelectRecord = await apis.show("TES1002", {"select": "name,email,password,_id"});
         // console.log('Show Select Record :: ', showSelectRecord);
 
         // Delete record by ID
-        // const deleteRecord = await apis.delete("EMP1003");
+        // const deleteRecord = await apis.delete("TES1003");
         // console.log('Delete Record :: ', deleteRecord);
 
         // Delete many records by IDs
-        // const deleteManyRecords = await apis.deleteMany("EMP1003,EMP1004");
+        // const deleteManyRecords = await apis.deleteMany("TES1003,TES1004");
         // console.log('Delete Many Records :: ', deleteManyRecords);
 
         // let data = {
@@ -106,7 +104,7 @@ const jsonSchema = require('./sample.json');
         // d.password = 'testing';
 
         // Update existing record
-        // let updatedRecord = await apis.update(id1, d);
+        // let updatedRecord = await apis.update(`${id1},${id2}`, d);
         // console.log('Updated Record :: ', updatedRecord);
 
     } catch (err) {
