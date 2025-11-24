c04ac2211bdded169febbe5c544c8cee5933d52a
Ravi Singh <ravisingh852937@gmail.com>
2025-11-24 20:04:44 +0530

    replace pg library with postgres library

--- drivers/pg.js
+++ drivers/pg.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const postgres = require('postgres');
 let log4js = require('log4js');
 
 const utils = require('../utils');
@@ -7,27 +7,21 @@ let version = require('../package.json').version;
 const logLevel = process.env.LOG_LEVEL || 'trace';
 const loggerName = process.env.HOSTNAME ? `[${process.env.DATA_STACK_NAMESPACE}] [${process.env.HOSTNAME}] [REST_CRUD PGSQL ${version}]` : `[REST_CRUD PGSQL ${version}]`;
 log4js.configure({
-    levels: {
-        AUDIT: { value: Number.MAX_VALUE - 1, colour: 'yellow' }
-    },
+    levels: { AUDIT: { value: Number.MAX_VALUE - 1, colour: 'yellow' } },
     appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
     categories: { default: { appenders: ['out'], level: logLevel.toUpperCase() } }
 });
 let logger = log4js.getLogger(loggerName);
 
+
 /**
  * @param {object} options CRUD options
- * @param {string} options.host
- * @param {string} options.user
- * @param {string} options.password
- * @param {string} options.database
- * @param {boolean} options.customId
- * @param {string} options.idPattern
  */
 function CRUD(options) {
     this.database = options.database;
     this.customId = options.customId || false;
     this.idPattern = options.idPattern || '';
+
     const connectionProps = {
         user: options.user,
         password: options.password,
@@ -36,58 +30,62 @@ function CRUD(options) {
         database: options.database,
         connectionString: options.connectionString,
         ssl: options.ssl,
-        types: options.types,
-        statement_timeout: options.statement_timeout,
-        query_timeout: options.query_timeout,
-        lock_timeout: options.lock_timeout,
-        application_name: options.application_name,
-        connectionTimeoutMillis: options.connectionTimeoutMillis,
-        keepAliveInitialDelayMillis: options.keepAliveInitialDelayMillis,
         idle_in_transaction_session_timeout: options.idle_in_transaction_session_timeout,
+        connectionTimeoutMillis: options.connectionTimeoutMillis,
         client_encoding: options.client_encoding,
+        application_name: options.application_name,
         fallback_application_name: options.fallback_application_name,
-        options: options.options,
-        idleTimeoutMillis: options.idleTimeoutMillis
     };
-    // logger.info('Connection props :: ', connectionProps);
+
     this.connectionDetails = Object.fromEntries(
-        Object.entries(connectionProps).filter(([_, v]) => v != null && v !== undefined)
+      Object.entries(connectionProps).filter(([_, v]) => v !== null && v !== undefined)
     );
 }
 
+
+/**
+ * Connect using postgres library
+ */
 CRUD.prototype.connect = async function () {
     try {
-        logger.debug('Connecting to PGSQL');
+        logger.debug('Connecting to PostgreSQL (postgres lib)');
         logger.trace(`Connection details :: ${JSON.stringify(this.connectionDetails)}`);
 
-        this.connection = await new Pool(this.connectionDetails);
-        this.connection.connect();
-        let result = await this.connection.query('SELECT 1 + 1 AS solution');
-        this.connection.on('connect', () => {
-            logger.trace(' Client connected to pool');
-        });
-        this.connection.on('remove', () => {
-            logger.trace(' Client removed from pool');
-        });
+        // Create postgres client
+        this.connection = postgres(
+          this.connectionDetails.connectionString ||
+          {
+              host: this.connectionDetails.host,
+              port: this.connectionDetails.port,
+              username: this.connectionDetails.user,
+              password: this.connectionDetails.password,
+              database: this.connectionDetails.database,
+              ssl: this.connectionDetails.ssl,
+              idle_timeout: this.connectionDetails.idle_in_transaction_session_timeout,
+              connect_timeout: this.connectionDetails.connectionTimeoutMillis
+          }
+        );
 
-        this.connection.on('error', (err) => {
-            logger.trace(' Unexpected error on idle client', err);
-        });
-        console.log('The solution is: ', result.rows[0].solution);
-        console.log('Connection Successfull!');
+        // Test connection
+        const result = await this.connection`SELECT 1 + 1 AS solution`;
+        console.log('The solution is:', result[0].solution);
+        console.log('Connection Successful!');
 
-        return 'Connection Successfull';
+        return 'Connection Successful';
     } catch (err) {
         logger.error('Error connecting :: ', err);
         throw err;
     }
-}
+};
 
+
+/**
+ * Disconnect postgres
+ */
 CRUD.prototype.disconnect = async function () {
     try {
-        this.connection.end();
+        await this.connection.end();
         console.log('Database Disconnected!');
-
         return 'Database Disconnected';
     } catch (err) {
         logger.error('Error disconnecting :: ', err);
@@ -95,20 +93,21 @@ CRUD.prototype.disconnect = async function () {
     }
 };
 
+
+/**
+ * Raw SQL query handler
+ */
 CRUD.prototype.sqlQuery = async function (sql, values) {
     try {
-        logger.debug(`Performing SQL Query`);
+        logger.debug('Performing SQL Query');
         logger.trace(`SQL Query :: ${sql}`);
 
-        if (!sql) {
-            logger.error('No sql query provided.');
-            throw new Error('No sql query provided.');
-        }
+        if (!sql) throw new Error('No sql query provided.');
 
-        let result = await this.connection.query(sql, values);
+        const result = await this.connection.unsafe(sql, values);
 
-        logger.trace(`Query result :: ${JSON.stringify(result.rows[0])}`);
-        return utils.unscapeData(result.rows);
+        logger.trace(`Query result :: ${JSON.stringify(result[0])}`);
+        return utils.unscapeData(result);
 
     } catch (err) {
         logger.error(`Error querying :: ${err}`);
@@ -118,65 +117,70 @@ CRUD.prototype.sqlQuery = async function (sql, values) {
 
 
 /**
- * @param {string} table
- * @param {object} jsonSchema
+ * Table wrapper
  */
 CRUD.prototype.table = function (table, jsonSchema) {
-    const options = {};
-    options.table = table;
-    options.database = this.database;
-    options.customId = this.customId;
-    options.idPattern = this.idPattern;
-    options.connection = this.connection;
-    return new Table(options, jsonSchema);
+    return new Table({
+        table,
+        database: this.database,
+        customId: this.customId,
+        idPattern: this.idPattern,
+        connection: this.connection
+    }, jsonSchema);
 };
+
+
+
+/******************* TABLE CLASS ********************/
 
 function Table(options, jsonSchema) {
     this.database = options.database;
-    this.customId = options.customId || false;
-    this.idPattern = options.idPattern || '';
+    this.customId = options.customId;
+    this.idPattern = options.idPattern;
     this.table = options.table;
     this.connection = options.connection;
     this.fields = utils.getFieldsFromSchema(jsonSchema);
-};
+}
 
+
+/**
+ * Create table
+ */
 Table.prototype.createTable = async function () {
     try {
         logger.debug(`Creating Table :: ${this.table}`);
 
-        let sql = utils.createTableStatement(this.fields);
-        logger.trace(`SQL query to create table :: ${`CREATE TABLE IF NOT EXISTS ${this.table}(${sql})`}`);
+        const sql = utils.createTableStatement(this.fields);
+        const query = `CREATE TABLE IF NOT EXISTS ${this.table}(${sql})`;
 
-        let tableResult = await this.connection.query(`CREATE TABLE IF NOT EXISTS ${this.table}(${sql})`);
+        logger.trace(`SQL :: ${query}`);
 
-        logger.debug(`Table created successfully`);
-        logger.trace(`Table created :: ${JSON.stringify(tableResult)}`);
+        await this.connection.unsafe(query);
 
+        logger.debug('Table created successfully');
         return 'Table created';
     } catch (err) {
         logger.error(`Error creating table :: ${err}`);
         throw err;
     }
-}
+};
 
+
+/**
+ * Count rows
+ */
 Table.prototype.count = async function (filter) {
     try {
-        logger.debug('Counting rows in DB.');
-        logger.trace(`Filter for count :: ${JSON.stringify(filter)}`);
-
+        logger.debug('Counting rows in DB');
         let sql = `SELECT count(*) AS count FROM ${this.table}`;
 
         const whereClause = utils.whereClause(this.fields, filter);
-        if (whereClause && whereClause.trim()) {
-            sql += whereClause;
-        }
+        if (whereClause) sql += whereClause;
 
-        logger.trace(`SQL Query to count :: ${sql}`);
+        logger.trace(`SQL :: ${sql}`);
 
-        let result = await this.connection.query(sql);
-
-        logger.debug(`Total records count :: ${result.rows[0].count}`);
-        return result.rows[0].count;
+        const result = await this.connection.unsafe(sql);
+        return result[0].count;
 
     } catch (err) {
         logger.error(`Error counting records :: ${err}`);
@@ -184,39 +188,28 @@ Table.prototype.count = async function (filter) {
     }
 };
 
+
+/**
+ * List rows
+ */
 Table.prototype.list = async function (options) {
     try {
-        logger.debug('Listing rows in DB.');
-        logger.trace(`Filters for listing :: ${JSON.stringify(options)}`);
+        logger.debug('Listing rows from DB');
 
-        let whereClause;
-        if (options?.filter && !_.isEmpty(options.filter)) {
-            whereClause = utils.whereClause(this.fields, options?.filter);
-        }
-
+        const whereClause = options?.filter ? utils.whereClause(this.fields, options.filter) : '';
         const selectClause = utils.selectClause(this.fields, options?.select) || '*';
         const limitClause = utils.limitClause(options?.count, options?.page);
         const orderByClause = utils.orderByClause(this.fields, options?.sort);
 
         let sql = `SELECT ${selectClause} FROM ${this.table}`;
-        if (whereClause) {
-            sql += whereClause;
-        }
-        if (orderByClause) {
-            sql += orderByClause;
-        }
-        if (limitClause) {
-            sql += limitClause;
-        }
+        if (whereClause) sql += whereClause;
+        if (orderByClause) sql += orderByClause;
+        if (limitClause) sql += limitClause;
 
-        logger.trace(`SQL query to list :: ${sql}`);
+        logger.trace(`SQL :: ${sql}`);
 
-        let result = await this.connection.query(sql);
-
-        logger.debug(`List query successful`);
-        logger.trace(`List of records :: ${JSON.stringify(utils.unscapeData(result.rows))}`);
-
-        return utils.unscapeData(result.rows);
+        const result = await this.connection.unsafe(sql);
+        return utils.unscapeData(result);
 
     } catch (err) {
         logger.error(`Error listing records :: ${err}`);
@@ -224,22 +217,21 @@ Table.prototype.list = async function (options) {
     }
 };
 
+
+/**
+ * Show a record
+ */
 Table.prototype.show = async function (id, options) {
     try {
-        logger.debug(`Fetching row from DB :: ${id}`);
-        logger.trace(`Filters for show :: ${JSON.stringify(options)}`);
+        logger.debug(`Fetching record :: ${id}`);
 
         const selectClause = utils.selectClause(this.fields, options?.select) || '*';
-        let sql = `SELECT ${selectClause} FROM ${this.table} WHERE _id='${id}'`;
+        const sql = `SELECT ${selectClause} FROM ${this.table} WHERE _id='${id}'`;
 
-        logger.trace(`SQL query for show :: ${sql}`);
+        logger.trace(`SQL :: ${sql}`);
 
-        let result = await this.connection.query(sql);
-
-        logger.debug(`Show record query auccessful`);
-        logger.trace(`Record details :: ${JSON.stringify(utils.unscapeData(result.rows[0]))}`);
-
-        return utils.unscapeData(result.rows[0]);
+        const result = await this.connection.unsafe(sql);
+        return utils.unscapeData(result[0]);
 
     } catch (err) {
         logger.error(`Error fetching record :: ${err}`);
@@ -247,117 +239,107 @@ Table.prototype.show = async function (id, options) {
     }
 };
 
+
+/**
+ * Create records
+ */
 Table.prototype.create = async function (data) {
     try {
-        logger.debug(`Creating new row in DB`);
-        logger.trace(`Data to create :: ${JSON.stringify(data)}`);
+        logger.debug('Creating new rows');
 
-        if (!data) {
-            throw new Error('No data to insert');
-        }
+        if (!data) throw new Error('No data to insert');
+        if (!Array.isArray(data)) data = [data];
 
-        if (!Array.isArray(data)) {
-            data = [data];
-        }
-
-        data.map(obj => {
-            if (!obj._id) {
-                obj._id = utils.token();
-            }
-        });
+        data.forEach(obj => { if (!obj._id) obj._id = utils.token(); });
 
         const stmt = utils.insertManyStatement(this.fields, data);
-        if (!stmt) {
-            throw new Error('No data to insert');
-        }
-        let sql1 = `INSERT INTO ${this.table} ${stmt}`;
+        if (!stmt) throw new Error('No data to insert');
 
-        logger.trace(`SQL query for insert :: ${sql1}`);
-        let result1 = await this.connection.query(sql1);
-        logger.debug(`Records created successfully`);
+        const insertSQL = `INSERT INTO ${this.table} ${stmt}`;
+        logger.trace(`SQL Insert :: ${insertSQL}`);
 
+        await this.connection.unsafe(insertSQL);
 
-        let sql2 = `SELECT * FROM ${this.table} WHERE _id IN (${data.map(obj => `'${obj._id}'`).join(`,`)})`;
+        const selectSQL =
+          `SELECT * FROM ${this.table} WHERE _id IN (${data.map(o => `'${o._id}'`).join(',')})`;
 
-        logger.trace(`SQL query for show :: ${sql2}`);
-        let result2 = await this.connection.query(sql2);
+        logger.trace(`SQL Select :: ${selectSQL}`);
 
+        const result = await this.connection.unsafe(selectSQL);
+        return utils.unscapeData(result);
 
-        logger.trace(`Records details :: ${JSON.stringify(utils.unscapeData(result2.rows))}`);
-        return utils.unscapeData(result2.rows);
     } catch (err) {
-        logger.error(`Error inserting/displaying records :: ${err}`);
+        logger.error(`Error in create :: ${err}`);
         throw err;
     }
 };
 
+
+/**
+ * Update
+ */
 Table.prototype.update = async function (id, data) {
     try {
-        logger.debug(`Updating row in DB :: ${id}`);
-        logger.trace(`Data to create :: ${JSON.stringify(data)}`);
-
-        if (!id) {
-            return reject(new Error('No id provided to update record'));
-        }
+        logger.debug(`Updating record :: ${id}`);
+        if (!id) throw new Error('No id provided');
 
         const stmt = utils.updateStatement(this.fields, data);
-        if (!stmt) {
-            throw new Error('data has no matching field to update');
-        }
-        let sql = `UPDATE ${this.table} ${stmt} WHERE _id IN (${id.split(',').map(i => `'${i}'`).join(',')})`;
+        if (!stmt) throw new Error('Invalid update payload');
 
-        logger.trace(`SQL query for update :: ${sql}`);
-        let result = await this.connection.query(sql);
+        const sql =
+          `UPDATE ${this.table} ${stmt} WHERE _id IN (${id.split(',').map(i => `'${i}'`).join(',')})`;
 
-        logger.debug(`Record updated successfully`);
-        logger.trace(`Updated record details :: ${JSON.stringify(result.rowCount)}`);
-        return result.rowCount;
+        logger.trace(`SQL :: ${sql}`);
+
+        const result = await this.connection.unsafe(sql);
+        return result.length;   // number of affected rows
 
     } catch (err) {
-        logger.error(`Error updating record :: ${err}`);
+        logger.error(`Error updating :: ${err}`);
         throw err;
     }
 };
 
+
+/**
+ * Delete
+ */
 Table.prototype.delete = async function (id) {
     try {
-        logger.debug(`Deleting record in DB :: ${id}`);
+        logger.debug(`Deleting record :: ${id}`);
+        if (!id) throw new Error('No id provided');
 
-        if (!id) {
-            throw new Error('No id provided to delete record');
-        }
-        let sql = `DELETE FROM ${this.table} WHERE _id='${id}'`;
+        const sql = `DELETE FROM ${this.table} WHERE _id='${id}'`;
+        logger.trace(`SQL :: ${sql}`);
 
-        logger.trace(`SQL query for delete :: ${sql}`);
-
-        let result = await this.connection.query(sql);
-
-        logger.debug(`Record deleted successfully`);
-        return result.rowCount;
+        const result = await this.connection.unsafe(sql);
+        return result.length;
 
     } catch (err) {
-        logger.error(`Error deleting record :: ${err}`);
+        logger.error(`Error deleting :: ${err}`);
         throw err;
     }
 };
 
+
+/**
+ * Delete Many
+ */
 Table.prototype.deleteMany = async function (ids) {
     try {
-        logger.debug(`Deleting multiple records from DB :: ${ids}`);
+        logger.debug(`Deleting multiple :: ${ids}`);
+        if (!ids) throw new Error('No ids provided');
 
-        if (!ids) {
-            return reject(new Error('No id provided to delete record'));
-        }
-        let sql = `DELETE FROM ${this.table} WHERE _id IN (${ids.split(',').map(id => `'${id}'`).join(',')})`;
+        const sql =
+          `DELETE FROM ${this.table} WHERE _id IN (${ids.split(',').map(id => `'${id}'`).join(',')})`;
 
-        logger.trace(`SQL query for deleting multiple records :: ${sql}`);
-        let result = await this.connection.query(sql);
+        logger.trace(`SQL :: ${sql}`);
 
-        logger.debug(`Records deleted successfully :: ${result.rowCount}`);
-        return result.rowCount;
+        const result = await this.connection.unsafe(sql);
+        return result.length;
 
     } catch (err) {
-        logger.error(`Error deleting multiple records :: ${err}`);
+        logger.error(`Error deleting multiple :: ${err}`);
         throw err;
     }
 };
